65438a5f535429003b97449a51a512236f040718
OlegChuy <misterchuy21@gmail.com>
2020-11-10 21:10:28 +0200

    began the play algorithm

--- docs/script.js
+++ docs/script.js
@@ -23,33 +23,50 @@ const createPlayfield = (size) => {
 const playfield = createPlayfield(8);
 
 const captureOponentCounters = (type, rowNum, start, end, userNum) => {
-  for (start; start < end; start++) {
-    const index = 8 * rowNum + start;
-    const li = counters[index];
-    console.log(index);
-    console.log(li);
-    const div = li.querySelector('div');
-    console.log(div);
-    if (userNum === 1) {
-      div.className = 'blackCounter';
-      playfield[rowNum][start] = 1;
-      quantityBlackCounters.innerText++;
-      quantityWhiteCounters.innerText--;
-    } else {
-      div.className = 'whiteCounter';
-      playfield[rowNum][start] = 2;
-      quantityWhiteCounters.innerText++;
-      quantityBlackCounters.innerText--;  
+  if (type === 'horizont') {
+    for (start; start < end; start++) {
+      const index = 8 * rowNum + start;
+      const li = counters[index];
+      const div = li.querySelector('div');
+      if (userNum === 1) {
+        div.className = 'blackCounter';
+        playfield[rowNum][start] = 1;
+        quantityBlackCounters.innerText++;
+        quantityWhiteCounters.innerText--;
+      } else {
+        div.className = 'whiteCounter';
+        playfield[rowNum][start] = 2;
+        quantityWhiteCounters.innerText++;
+        quantityBlackCounters.innerText--;  
+      }
+    }
+  } else if (type === 'vertical') {
+    for (start; start < end; start++) {
+      const index = 8 * start + rowNum;
+      const li = counters[index];
+      const div = li.querySelector('div');
+      if (userNum === 1) {
+        div.className = 'blackCounter';
+        playfield[start][rowNum] = 1;
+        quantityBlackCounters.innerText++;
+        quantityWhiteCounters.innerText--;
+      } else {
+        div.className = 'whiteCounter';
+        playfield[start][rowNum] = 2;
+        quantityWhiteCounters.innerText++;
+        quantityBlackCounters.innerText--;  
+      }
     }
   }
 }
 
 const checkPossibleMovements = (possibleRow, possibleCol, userNum) => {
   for (let row = 0; row < playfield.length; row++) {
-    for (let col = 0; col < playfield.length; col++) {
+    column:for (let col = 0; col < playfield.length; col++) {
       if (row === possibleRow && playfield[row][col] === userNum) {
       	const activeRow = playfield[row];
       	let start = null;
+        let end = null;
       	if (col === possibleCol) continue;
       	if (col > possibleCol) {
       		start = possibleCol + 1;
@@ -60,16 +77,48 @@ const checkPossibleMovements = (possibleRow, possibleCol, userNum) => {
         }
         if (start === end) continue;
        	for (let index = start; index < end; index++) {
-      	  if (activeRow[index] === 0 || activeRow[index] === userNum) continue;
+      	  if (activeRow[index] === 0 || activeRow[index] === userNum) continue column;
       	}
-        console.log(start);
-        console.log(end);
         captureOponentCounters('horizont', row, start, end, userNum);
         return true;
+      }
+      const activeColumn = [];
+      for (let num = 0; num < 8; num++) {
+        activeColumn.push(playfield[possibleCol][num]);
+      }
+      if (row === possibleCol && playfield[col][row] === userNum) {
+        let start = null;
+        let end = null;
+        console.log(possibleCol);
+        console.log(possibleRow);
+        console.log(col);
+        console.log(row);
+        if (col === possibleRow) console.log('HRE');
+        if (col > possibleRow) {
+          start = possibleRow + 1;
+          end = col;
+        } else {
+          start = col + 1;
+          end = possibleRow;
+        }
+        console.log(start);
+        console.log(end);
+        if (start != end) {
+          console.log('Here');
+          for (let index = start; index < end; index++) {
+            if (activeColumn[index] === 0 || activeColumn[index] === userNum) continue column;
+          }
+          captureOponentCounters('vertical', row, start, end, userNum);
+          return true;
+        }
       }
     }
   }
 }
+
+// remake 'if' conditions in functions
+// refactor functions 
+// think about 'continue'
 
 counters.forEach(counter => {
   const div = counter.querySelector('div');
