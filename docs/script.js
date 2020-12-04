6ca2261d12c4efe0a10ee06242a2b2976e23cd81
OlegChuy <misterchuy21@gmail.com>
2020-12-05 00:24:28 +0200

    finished game logic

--- docs/script.js
+++ docs/script.js
@@ -4,8 +4,10 @@ const diceNames = ['img/dice_one.svg', 'img/dice_two.svg', 'img/dice_three.svg',
 const rollDiceButton = document.querySelector('.rollDice');
 const attempsNum = document.querySelector('.attempsNum');
 const dices = document.querySelectorAll('.diceRow div');
-const playerColumn = document.querySelectorAll('.player');
+const userColumn = document.querySelectorAll('.player');
 const computerColumn = document.querySelectorAll('.computer');
+const userResult = document.querySelector('.playerTotal');
+const computerResult = document.querySelector('.computerTotal');
 let player = 'user';
 
 const randomFromTo = (from, to) => Math.floor(from + (Math.random() * (to - from + 1)));
@@ -22,15 +24,33 @@ const generateDices = () => {
   return newDices;
 }
 
+const clearDiceRow = () => {
+	const endDiceNum = (player === 'user') ? 10 : 20;
+	const startDiceNum = (player === 'user') ? 0 : 10;
+  for (let index = startDiceNum; index < endDiceNum; index++) {
+    dices[index].style.background = 'white';
+  }
+};
+
+const countResult = () => {
+	const resultColumn = (player === "user") ? userResult : computerResult;
+	const activeColumn = (player === "user") ? userColumn : computerColumn;
+  let result = 0;
+  activeColumn.forEach(column => {
+    if (column.id === 'close') result += Number.parseInt(column.innerText);
+  });
+  resultColumn.innerText = result;
+};
+
 const checkNumbers = (numbersArr) => {
-  let result = [0, 0, 0, 0, 0, 0];
+  const result = [0, 0, 0, 0, 0, 0];
   numbersArr.forEach(number => {
-    if (number == 1) result[0] += 1;
-    if (number == 2) result[1] += 2;
-    if (number == 3) result[2] += 3;
-    if (number == 4) result[3] += 4;
-    if (number == 5) result[4] += 5;
-    if (number == 6) result[5] += 6;
+    if (number == 1) result[0] ++;
+    if (number == 2) result[1] ++;
+    if (number == 3) result[2] ++;
+    if (number == 4) result[3] ++;
+    if (number == 5) result[4] ++;
+    if (number == 6) result[5] ++;
   });
   return result;
 };
@@ -39,23 +59,48 @@ const checkStraight = (numbersArr, rollNum) => {
   let result = 0;
   for (let num = 1; num <= 5; num++) {
     if (!(numbersArr.includes(`${num}`))) break;
-    if (num === 5) result = (rollNum === 3) ? 25 : 20; 
+    if (num === 5) result = (rollNum === '3') ? 25 : 20; 
   }
   for (let num = 2; num <= 6; num++) {
     if (!(numbersArr.includes(num))) break;
-    if (num === 6) result = (rollNum === 3) ? 25 : 20;
+    if (num === 6) result = (rollNum === '3') ? 25 : 20;
   }
   return result;
 };
 
-const checkFullHouse = (numbersArr, rollNum) => {
-  
+const checkFullHouse = (numbers, rollNum) => {
+  let result = 0;
+  if (numbers.includes(2) && numbers.includes(3)) result = (rollNum === '3') ? 35 : 30;
+  return result;
 };
 
+const checkFourOneType = (numbers, rollNum) => {
+  let result = 0;
+  if (numbers.includes(4)) result = (rollNum === '3') ? 45 : 40;
+  return result;
+};
+
+const checkGeneral = (numbers, rollNum) => {
+  let result = 0;
+  if (numbers.includes(5)) result = (rollNum === '3') ? 1000 : 60;
+  return result;
+}
+
 const checkCombinations = (numbersArr, rollNum) => {
-  const activeColumn = (player === 'user' ? playerColumn : computerColumn);
+  const activeColumn = (player === 'user' ? userColumn : computerColumn);
+  const result = [];
   const numbers = checkNumbers(numbersArr);
-  const straight = checkStraight(numbersArr, rollNum);
+  numbers.map((number, index) => result.push(number * (index + 1)));
+  result.push(checkStraight(numbersArr, rollNum));
+  result.push(checkFullHouse(numbers, rollNum));
+  result.push(checkFourOneType(numbers, rollNum));
+  result.push(checkGeneral(numbers, rollNum));
+  activeColumn.forEach((column, index) => {
+    if (column.id != 'close') { 
+      column.innerText = result[index];
+      column.style.background = 'yellow';
+    }
+  });
 };
 
 const rollDice = () => {
@@ -80,20 +125,48 @@ const rollDice = () => {
 const save_deleteDice = (dice, index) => {
   const diceIndex = index % 5;
   const diceClass = dice.classList[0];
-  
+
   if (savedDices[diceIndex] === null && diceClass === 'activeDice') {
     savedDices[diceIndex] = dice.id;
     dice.style.background = 'white';
-    const diceImageName = diceNames[savedDices[diceIndex] - 1];    
-    dices[index - 5].style.background = `url(${diceImageName})`;
+    const diceImageName = diceNames[savedDices[diceIndex] - 1];
+    const diceNum = (player === 'user') ? index - 5 : index + 5;
+    dices[diceNum].style.background = `url(${diceImageName})`;
   } else if (savedDices[diceIndex] != null && diceClass === 'savedDice') {
     const diceImageName = diceNames[savedDices[diceIndex] - 1];
     savedDices[diceIndex] = null;
     dice.style.background = 'white';
-    dices[index + 5].style.background = `url(${diceImageName})`;
+    const diceNum = (player === 'user') ? index + 5 : index - 5;
+    dices[diceNum].style.background = `url(${diceImageName})`;
   }
 
 }
 
+const saveValueInTable = column => {
+  if (column.style['background-color'] === 'yellow') {
+    column.id = 'close';
+    column.style.background = '#e62d47';
+    const activeColumn = (player === "user") ? userColumn : computerColumn; 
+    activeColumn.forEach(column => {
+      
+      if (column.id != 'close') {
+      	column.innerText = 0;
+      	column.style.background = '#9c9dce';
+      }
+     
+    });
+
+    clearDiceRow();
+    countResult();
+    for (const key in savedDices) {
+      savedDices[key] = null;
+    }
+	  player = (player === 'user') ? 'computer' : 'user';
+	  attempsNum.innerText = 3;
+  }
+};
+
 rollDiceButton.addEventListener('click', rollDice);
 dices.forEach((dice, index) => dice.addEventListener('click', save_deleteDice.bind(null, dice, index)));
+userColumn.forEach((column, index) => column.addEventListener('click', saveValueInTable.bind(null, column)));
+computerColumn.forEach((column, index) => column.addEventListener('click', saveValueInTable.bind(null, column)));
